0951a0696927b133af0e54a052267ff486a0e64b
FriedRubyjane <wangjinhyun@gmail.com>
2024-04-09 18:27:21 +0300

    completed loginform

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { FC, forwardRef, HTMLProps } from 'react'
+import { FC, HTMLProps } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
 import styles from './Input.module.scss'
@@ -13,37 +13,27 @@ interface InputProps extends HTMLProps<HTMLInputElement> {
   validationRules?: RegisterOptions
 }
 
-export const Input: FC<InputProps> = forwardRef<
-  HTMLInputElement,
-  InputProps
->(
-  (
-    {
-      title,
-      placeholder,
-      id,
-      name,
-      type,
-      register,
-      validationRules,
-      ...rest
-    },
-    ref,
-  ) => {
-    return (
-      <div className={styles.wrapper}>
-        <label className={styles.title} htmlFor={id}>
-          {title}
-        </label>
-        <input
-          {...rest}
-          className={styles.input}
-          placeholder={placeholder}
-          id={id}
-          type={type}
-          {...register(name, validationRules)}
-        />
-      </div>
-    )
-  },
-)
+export const Input: FC<InputProps> = ({
+  title,
+  placeholder,
+  id,
+  name,
+  type,
+  register,
+  validationRules,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.title} htmlFor={id}>
+        {title}
+      </label>
+      <input
+        className={styles.input}
+        placeholder={placeholder}
+        id={id}
+        type={type}
+        {...register(name, validationRules)}
+      />
+    </div>
+  )
+}
