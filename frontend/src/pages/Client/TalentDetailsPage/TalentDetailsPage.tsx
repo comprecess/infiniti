060263fb19437e88ed3c9562cbcd5db8a5c32da7
FriedRubyjane <wangjinhyun@gmail.com>
2024-06-10 17:16:28 +0300

    update pages (offers, invoices) && update talent details

--- frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
+++ frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
@@ -1,16 +1,13 @@
 import { FC, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 import styles from './TalentDetailsPage.module.scss'
 
 export const TalentPage: FC = () => {
-  const navigate = useNavigate()
-
   useEffect(() => {
     document.title = 'infiniti | Talent Details'
 
     window.scrollTo(0, 0)
   }, [])
 
-  return <div className={styles.wrapper}>d</div>
+  return <div className={styles.wrapper}>A</div>
 }
