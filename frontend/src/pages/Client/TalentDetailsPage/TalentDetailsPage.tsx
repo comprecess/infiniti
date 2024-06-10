10134fba04696d18f29f50bd062c63d4525a38ec
FriedRubyjane <wangjinhyun@gmail.com>
2024-06-10 17:20:41 +0300

    update pages (offers, invoices) && update talent details

--- frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
+++ frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
@@ -2,6 +2,10 @@ import { FC, useCallback, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Routes } from '../../../app/router/routes'
+import { AboutTalentCard } from '../../../features/Client/TalentDetailsPage/AboutTalentCard/AboutTalentCard'
+import { EducationCard } from '../../../features/Client/TalentDetailsPage/EducationCard/EducationCard'
+import { ProjectsExperienceCard } from '../../../features/Client/TalentDetailsPage/ProjectsExperienceCard/ProjectsExperienceCard'
+import { TalentCard } from '../../../features/Client/TalentDetailsPage/TalentCard/TalentCard'
 import { ChevronDownIcon } from '../../../shared/icons/ChevronDownIcon'
 import styles from './TalentDetailsPage.module.scss'
 
@@ -25,6 +29,16 @@ export const TalentPage: FC = () => {
           <div className={styles.buttonBack} onClick={handleNavigateBack}>
             <ChevronDownIcon style={styles.buttonBackIcon} />
             <span className={styles.buttonBackText}>Back</span>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div className={styles.item} style={{ height: '100%' }}>
+          <TalentCard />
+          <div className={styles.info}>
+            <AboutTalentCard />
+            <ProjectsExperienceCard />
+            <EducationCard />
           </div>
         </div>
       </section>
