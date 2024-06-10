7e96fd5e13935e4a1ceea95e04b1a869bab389ab
FriedRubyjane <wangjinhyun@gmail.com>
2024-06-10 17:14:07 +0300

    update pages (offers, invoices) && update talent details

--- frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
+++ frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
@@ -1,13 +1,6 @@
-import { FC, useCallback, useEffect } from 'react'
+import { FC, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { Routes } from '../../../app/router/routes'
-import { AboutTalentCard } from '../../../features/Client/TalentDetailsPage/AboutTalentCard/AboutTalentCard'
-import { EducationCard } from '../../../features/Client/TalentDetailsPage/EducationCard/EducationCard'
-import { ProjectsExperienceCard } from '../../../features/Client/TalentDetailsPage/ProjectsExperienceCard/ProjectsExperienceCard'
-import { SimilarTalents } from '../../../features/Client/TalentDetailsPage/SimilarTalents/SimilarTalents'
-import { TalentCard } from '../../../features/Client/TalentDetailsPage/TalentCard/TalentCard'
-import { ChevronDownIcon } from '../../../shared/icons/ChevronDownIcon'
 import styles from './TalentDetailsPage.module.scss'
 
 export const TalentPage: FC = () => {
@@ -19,42 +12,5 @@ export const TalentPage: FC = () => {
     window.scrollTo(0, 0)
   }, [])
 
-  const handleNavigateBack = useCallback(() => {
-    navigate('/' + Routes.clientPages + '/' + Routes.catalog)
-  }, [])
-
-  return (
-    <div className={styles.wrapper}>
-      <section className={styles.section}>
-        <div className={styles.item}>
-          <div className={styles.buttonBack} onClick={handleNavigateBack}>
-            <ChevronDownIcon style={styles.buttonBackIcon} />
-            <span className={styles.buttonBackText}>Back</span>
-          </div>
-        </div>
-      </section>
-      <section className={styles.section}>
-        <div className={styles.item} style={{ height: '100%' }}>
-          <TalentCard />
-          <div className={styles.info}>
-            <AboutTalentCard />
-            <ProjectsExperienceCard />
-            <EducationCard />
-          </div>
-        </div>
-      </section>
-      <section className={styles.section}>
-        <div className={styles.item}>
-          <SimilarTalents />
-        </div>
-      </section>
-      {
-        //<section className={styles.section}>
-        //<div className={styles.item}>
-        //<ButtonBrand title='Back to top' onClick={scrollToTop} />
-        //</div>
-        //</section>
-      }
-    </div>
-  )
+  return <div className={styles.wrapper}>d</div>
 }
