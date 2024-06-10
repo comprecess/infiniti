a574837e0a10516b68a95d4786910a104c2accc3
FriedRubyjane <wangjinhyun@gmail.com>
2024-06-10 17:18:34 +0300

    update pages (offers, invoices) && update talent details

--- frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
+++ frontend/src/pages/Client/TalentDetailsPage/TalentDetailsPage.tsx
@@ -1,13 +1,33 @@
-import { FC, useEffect } from 'react'
+import { FC, useCallback, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import { Routes } from '../../../app/router/routes'
+import { ChevronDownIcon } from '../../../shared/icons/ChevronDownIcon'
 import styles from './TalentDetailsPage.module.scss'
 
 export const TalentPage: FC = () => {
+  const navigate = useNavigate()
+
   useEffect(() => {
     document.title = 'infiniti | Talent Details'
 
     window.scrollTo(0, 0)
   }, [])
 
-  return <div className={styles.wrapper}>A</div>
+  const handleNavigateBack = useCallback(() => {
+    navigate('/' + Routes.clientPages + '/' + Routes.catalog)
+  }, [])
+
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.section}>
+        <div className={styles.item}>
+          <div className={styles.buttonBack} onClick={handleNavigateBack}>
+            <ChevronDownIcon style={styles.buttonBackIcon} />
+            <span className={styles.buttonBackText}>Back</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
