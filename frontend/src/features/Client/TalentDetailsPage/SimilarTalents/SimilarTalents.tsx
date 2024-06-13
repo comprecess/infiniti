22dfd4db8dbfd7fb50129478466b0ba0d735385a
FriedRubyjane <wangjinhyun@gmail.com>
2024-06-13 20:21:57 +0300

    talents get backend

--- frontend/src/features/Client/TalentDetailsPage/SimilarTalents/SimilarTalents.tsx
+++ frontend/src/features/Client/TalentDetailsPage/SimilarTalents/SimilarTalents.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react'
 
-import { TalentsListData } from '../../../../app/data/client/talentsList'
-import { TalentsCard } from '../../../../widgets/TalentsCard/TalentsCard'
 import styles from './SimilarTalents.module.scss'
 
 export const SimilarTalents: FC = () => {
@@ -9,9 +7,9 @@ export const SimilarTalents: FC = () => {
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Similar talents</h2>
       <div className={styles.list}>
-        <TalentsCard talentInfo={TalentsListData[5]} />
-        <TalentsCard talentInfo={TalentsListData[12]} />
-        <TalentsCard talentInfo={TalentsListData[20]} />
+        {/* <TalentsCard talentInfo={TalentsListData[5]} /> */}
+        {/* <TalentsCard talentInfo={TalentsListData[12]} /> */}
+        {/* <TalentsCard talentInfo={TalentsListData[20]} /> */}
       </div>
     </div>
   )
