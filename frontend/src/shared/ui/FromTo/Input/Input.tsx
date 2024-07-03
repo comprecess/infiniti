f667d033e3ffd9d908a96346520c304819ca9d78
FriedRubyjane <wangjinhyun@gmail.com>
2024-07-03 11:42:57 +0300

    all filters in catalog

--- frontend/src/shared/ui/FromTo/Input/Input.tsx
+++ frontend/src/shared/ui/FromTo/Input/Input.tsx
@@ -1,24 +1,32 @@
-import { FC } from 'react'
+import { ChangeEvent, FC } from 'react'
 
 import styles from './Input.module.scss'
 
 interface InputProps {
   placeholder: string
+  isFilters?: boolean
   tabIndex?: number
+  value?: string
   style?: string
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
 export const Input: FC<InputProps> = ({
   placeholder,
+  isFilters = false,
   tabIndex,
+  value,
   style,
+  onChange,
 }) => {
   return (
     <input
       type='number'
+      value={value}
       placeholder={placeholder}
       className={`${styles.input} ${style}`}
       tabIndex={tabIndex}
+      onChange={isFilters ? onChange : undefined}
     />
   )
 }
