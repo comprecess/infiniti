6190860af2feaf232d6e8978a6d05b02eb4ebfc9
FriedRubyjane <wangjinhyun@gmail.com>
2024-07-15 15:28:49 +0300

    add drag and drop in groups reorder

---
+++ frontend/src/features/Admin/CustomersPage/GroupsPage/RecentReorderGroups/RecentReorderGroups.tsx
@@ -0,0 +1,61 @@
+import { FC, useState } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+import { GroupsListProps } from '../../../../../app/constants/constants'
+import { Item } from './Item/Item'
+import styles from './RecentReorderGroups.module.scss'
+
+interface RecentReorderGroupsProps {
+  groupsList: GroupsListProps[]
+  ReRequestGetGroups: (listId: number[]) => void
+}
+
+export const RecentReorderGroups: FC<RecentReorderGroupsProps> = ({
+  groupsList,
+  ReRequestGetGroups,
+}) => {
+  const [idList, setIdList] = useState(groupsList.map(item => item.id))
+  const [items, setItems] = useState(groupsList)
+
+  const moveItem = (dragIndex: number, hoverIndex: number) => {
+    const updatedItems = [...items]
+    const updatedIds = [...idList]
+
+    const dragItem = updatedItems[dragIndex]
+    updatedItems[dragIndex] = updatedItems[hoverIndex]
+    updatedItems[hoverIndex] = dragItem
+
+    const dragId = updatedIds[dragIndex]
+    updatedIds[dragIndex] = updatedIds[hoverIndex]
+    updatedIds[hoverIndex] = dragId
+
+    setIdList(updatedIds)
+    setItems(updatedItems)
+  }
+
+  const handleSort = () => {
+    ReRequestGetGroups(idList)
+  }
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className={styles.wrapper}>
+        <h5 className={styles.title}>
+          Drag & drop the Items below for Repositioning. Click to Edit.
+        </h5>
+        <div className={styles.list}>
+          {items.map((item, index) => (
+            <Item
+              key={item.id}
+              index={index}
+              name={item.name}
+              moveItem={moveItem}
+              sort={handleSort}
+            />
+          ))}
+        </div>
+      </div>
+    </DndProvider>
+  )
+}
--- frontend/src/pages/Admin/CustomersPage/GroupsPage/ReorderGroupsPage/ReorderGroupsPage.tsx
+++ frontend/src/pages/Admin/CustomersPage/GroupsPage/ReorderGroupsPage/ReorderGroupsPage.tsx
@@ -1,7 +1,66 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
+import { GroupsListProps } from '../../../../../app/constants/constants'
+import { RecentReorderGroups } from '../../../../../features/Admin/CustomersPage/GroupsPage/RecentReorderGroups/RecentReorderGroups'
+import { useCustomToast } from '../../../../../shared/ui/CustomToast/CustomToast'
+import { LoadingSpinner } from '../../../../../shared/ui/LoadingSpinner/LoadingSpinner'
+import { getListGroups } from '../../../../../shared/utils/api/Groups/GetGroups'
+import { sortGroups } from '../../../../../shared/utils/api/Groups/SortGroups'
+import { RecentCard } from '../../../../../widgets/RecentCard/RecentCard'
 import styles from './ReorderGroupsPage.module.scss'
 
 export const AdminReorderGroupsPage: FC = () => {
-  return <div className={styles.wrapper}>Admin Reorder Groups Page</div>
+  const [groups, setGroups] = useState<GroupsListProps[]>([])
+
+  const showToast = useCustomToast()
+
+  const getGroups = async () => {
+    const groupsResponse: GroupsListProps[] = await getListGroups()
+
+    setGroups(groupsResponse)
+  }
+
+  const sortGroupsList = async (listId: number[]) => {
+    const sortResponse = await sortGroups(listId)
+
+    if (sortResponse) {
+      showToast({
+        title: 'Successfully',
+        description: 'You have successfully changed the group order',
+        status: 'success',
+      })
+
+      getGroups()
+    } else {
+      showToast({
+        title: 'Error',
+        description: 'Error when changing group order',
+        status: 'error',
+      })
+    }
+  }
+
+  useEffect(() => {
+    getGroups()
+  }, [])
+
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.section}>
+        {groups.length > 0 ? (
+          <RecentCard
+            title='Reorder Groups Positions'
+            style={styles.recentFullScreen}
+          >
+            <RecentReorderGroups
+              groupsList={groups}
+              ReRequestGetGroups={sortGroupsList}
+            />
+          </RecentCard>
+        ) : (
+          <LoadingSpinner size='xl' />
+        )}
+      </section>
+    </div>
+  )
 }
