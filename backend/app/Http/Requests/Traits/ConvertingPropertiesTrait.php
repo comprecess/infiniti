02cfbbe017129570261b24969f0a4b31df26c8eb
Igor <crash.strelkov@gmail.com>
2024-07-18 21:10:33 +0400

    convert form

--- backend/app/Http/Requests/Traits/ConvertingPropertiesTrait.php
+++ backend/app/Http/Requests/Traits/ConvertingPropertiesTrait.php
@@ -4,45 +4,21 @@ namespace App\Http\Requests\Traits;
 trait ConvertingPropertiesTrait
 {
 
-//    public function conversion()
-//    {
-//        foreach ($this->getListProperties() as $key => $val)
-//        {
-//            $value = is_int($key) ? $this->{$val} : $this->{$key};
-//            if(!is_null($value)) {
-//                $this->{$val} = $value;
-//            }
-//        }
-//    }
-
     public function conversion()
     {
-        $list = $this->getListProperties();
-        foreach ($this->all() as $key => $val)
+        foreach ($this->getListProperties() as $key => $val)
         {
-            $key = $list[$key] ?? $key;
-            $this->{$key} = $val;
+            $value = is_int($key) ? $this->{$val} : $this->{$key};
+            $this->{$val} = $value;
         }
     }
 
-//    public function setModel($model)
-//    {
-//        foreach ($this->getListProperties() as $key => $val)
-//        {
-//            $value = is_int($key) ? $this->{$val} : $this->{$key};
-//            if(!is_null($value)) {
-//                $model->{$val} = $value;
-//            }
-//        }
-//    }
-
     public function setModel($model)
     {
-        $list = $this->getListProperties();
-        foreach ($this->all() as $key => $val)
+        foreach ($this->getListProperties() as $key => $val)
         {
-            $key = $list[$key] ?? $key;
-            $model->{$key} = $val;
+            $value = is_int($key) ? $this->{$val} : $this->{$key};
+            $model->{$val} = $value;
         }
     }
 }
