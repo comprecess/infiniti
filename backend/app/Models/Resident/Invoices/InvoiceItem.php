4e3a05e98b2849306c440db4d15dbe4ae0650034
Igor <crash.strelkov@gmail.com>
2024-09-13 13:11:08 +0400

    blank, individual requests

--- backend/app/Models/Resident/Invoices/InvoiceItem.php
+++ backend/app/Models/Resident/Invoices/InvoiceItem.php
@@ -40,6 +40,11 @@ class InvoiceItem extends Model implements InsertDefaultValueInterface
         return $this->belongsTo(Tax::class, 'tax_rate', 'rate');
     }
 
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class, 'invoiceid');
+    }
+
     public function getDefault(): array
     {
         return [
@@ -63,12 +68,13 @@ class InvoiceItem extends Model implements InsertDefaultValueInterface
         return array_flip(self::DISCOUNT_TYPE)[$this->discount_type ?? 'f'];
     }
 
+    //notService
     public function getSumm()
     {
-        return intval($this->qty) * $this->amount;
+        return round(intval($this->qty) * $this->amount);
     }
 
-
+    //notService
     public function getDiscount()
     {
         if(!$this->discount_amount) {
@@ -82,6 +88,33 @@ class InvoiceItem extends Model implements InsertDefaultValueInterface
         }
 
         return $this->discount_amount;
+    }
+
+    public function calc()
+    {
+//        $service = $this->service;
+//        $price = $this->amount ?? 0;
+//        $amount = $this->qty ? $this->qty : 0;
+//        if($service) {
+//            $price = $service->getPrice();
+//            $this->amo
+//        }
+//        $priceAmount = round($amount * $price, 2);
+//
+//        if($this->discount_type == 'p') {
+//            $value = $this->discount_amount < 0 ? 0 : ($this->discount_amount > 100 ? 100 : $this->discount_amount);
+//        }
+
+        $priceAmount = $this->getSumm();
+        $discount = $this->getDiscount();
+        $total = round($priceAmount - $discount, 2);
+
+        if($this->tax_rate) {
+            $this->taxamount = round($total * ($this->tax_rate * 0.01), 2);
+            $total += $this->taxamount;
+        }
+
+        $this->total = $total;
     }
 
 }
