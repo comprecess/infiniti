e05aa54d52a55a046402978837ff9aa5468627c8
FriedRubyjane <trufflecooperation@gmail.com>
2024-09-15 17:54:34 +0300

    fix invoice blanks

--- frontend/src/features/Admin/Sales/EditInvoice/Fields/Fields.tsx
+++ frontend/src/features/Admin/Sales/EditInvoice/Fields/Fields.tsx
@@ -50,12 +50,12 @@ interface InfoData {
 
 export interface PartialFieldsData extends Partial<InfoData> {
   [key: string]:
-    | string
-    | number
-    | SalesEditInvoiceBlankData[]
-    | BlankCalc
-    | undefined
-    | null
+  | string
+  | number
+  | SalesEditInvoiceBlankData[]
+  | BlankCalc
+  | undefined
+  | null
 }
 
 export const Fields: FC<FieldsProps> = ({
@@ -102,11 +102,11 @@ export const Fields: FC<FieldsProps> = ({
   const handleChangeInput = (
     field: string,
     value:
-      | string
-      | number
-      | SalesEditInvoiceBlankData[]
-      | undefined
-      | null,
+    | string
+    | number
+    | SalesEditInvoiceBlankData[]
+    | undefined
+    | null,
   ) => {
     let updatedValue = value
 
