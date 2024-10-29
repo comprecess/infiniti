ccbef214c994da79ddb8f018711a54ad0338644a
Igor <crash.strelkov@gmail.com>
2024-10-29 14:24:35 +0400

    fix

--- backend/app/Http/Resources/Resident/Talents/TalentPropResource.php
+++ backend/app/Http/Resources/Resident/Talents/TalentPropResource.php
@@ -17,11 +17,21 @@ class TalentPropResource extends PropertyResorce
 //        $this->setList($resource);
 //        $resource['values'] = ValueResorce::collection($this->userValue);
         //or
+        $this->validValue();
         $resource[snakeCaseToPascalCase($this->id_name)] = ValueResorce::collection($this->userValue);
 
 
 
         return $resource;
+    }
+
+    public function validValue()
+    {
+        if($this->id_name == 'rate') {
+            $this->userValue->each(function(&$item){
+                $item->value = (bool) $item->value;
+            });
+        }
     }
 
 
