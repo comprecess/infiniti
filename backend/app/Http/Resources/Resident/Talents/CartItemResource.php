b02bf5fc10b74326dbcbe494e775a7c04d2365f4
Igor <crash.strelkov@gmail.com>
2024-11-05 18:37:26 +0400

    cart item

--- backend/app/Http/Resources/Resident/Talents/CartItemResource.php
+++ backend/app/Http/Resources/Resident/Talents/CartItemResource.php
@@ -18,6 +18,8 @@ class CartItemResource extends JsonResource
             'total' => $this->total,
             'nameType' => $this->name_id_type,
             'price' => $this->price,
+            'taxesInclude' => $this->taxes_include,
+            'tax' => $this->getTaxesTotalPrice()
         ];
     }
 
