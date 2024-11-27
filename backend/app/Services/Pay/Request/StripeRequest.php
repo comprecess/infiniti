a91cd80c2f38dc1379dc496a24280f66c6d1efb8
Igor <crash.strelkov@gmail.com>
2024-11-27 11:30:14 +0400

    stripe pay back

--- backend/app/Services/Pay/Request/StripeRequest.php
+++ backend/app/Services/Pay/Request/StripeRequest.php
@@ -10,7 +10,7 @@ class StripeRequest extends FormRequest
     public function rules(): array
     {
         return [
-//            'token' => 'required'
+            'token' => 'required'
         ];
     }
 }
