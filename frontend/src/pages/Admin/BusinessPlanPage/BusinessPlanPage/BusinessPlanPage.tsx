78e5e26e1e5bb9b43cd87549633c1268bd70fb99
Jme Truffle <trufflecooperation@gmail.com>
2024-12-06 18:24:12 +0300

    business plan

--- frontend/src/pages/Admin/BusinessPlanPage/BusinessPlanPage/BusinessPlanPage.tsx
+++ frontend/src/pages/Admin/BusinessPlanPage/BusinessPlanPage/BusinessPlanPage.tsx
@@ -1,9 +1,40 @@
 import { FC, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { Routes } from '../../../../app/router/routes'
+import { CardPlan } from '../../../../features/Admin/BusinessPlanPage/BusinessPlanPage/CardPlan/CardPlan'
+import { ButtonBlue } from '../../../../shared/ui/ButtonBlue/ButtonBlue'
+import styles from './BusinessPlanPage.module.scss'
 
 export const AdminBusinessPlanPage: FC = () => {
+  const navigate = useNavigate()
+
+  const handleNavigateToMakeBusinessPlan = () => {
+    navigate(
+      `/${Routes.adminPages}/${Routes.businessPlan}/${Routes.makeBusinessPlan}`,
+    )
+  }
+
   useEffect(() => {
     document.title = 'infiniti | Business Plan'
   }, [])
 
-  return <div>Admin Business Plan Page</div>
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.section}>
+        <div className={styles.wrapperButtonBlue}>
+          <ButtonBlue
+            title='Make Business Plan'
+            style={styles.buttonBlue}
+            onClick={handleNavigateToMakeBusinessPlan}
+          />
+        </div>
+        <div className={styles.plans}>
+          <CardPlan />
+          <CardPlan />
+          <CardPlan />
+        </div>
+      </section>
+    </div>
+  )
 }
---
+++ frontend/src/pages/Admin/BusinessPlanPage/EditBusinessPlanPage/EditBusinessPlanPage.tsx
@@ -0,0 +1,27 @@
+import { Fields } from '../../../../features/Admin/BusinessPlanPage/EditBusinessPlanPage/Fields/Fields'
+import { ButtonBlue } from '../../../../shared/ui/ButtonBlue/ButtonBlue'
+import { RecentCard } from '../../../../widgets/RecentCard/RecentCard'
+import styles from './EditBusinessPlanPage.module.scss'
+
+export const AdminEditBusinessPlanPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.section}>
+        <RecentCard
+          title='Edit Business Plan'
+          style={styles.recentFullScreen}
+          Component={ButtonBlue}
+          componentProps={{
+            titleNone: true,
+            title: 'Save',
+            style: styles.buttonSave,
+            iconProps: styles.buttonSaveIcon,
+            icon: '/icons/fileWhite.svg',
+          }}
+        >
+          <Fields />
+        </RecentCard>
+      </section>
+    </div>
+  )
+}
