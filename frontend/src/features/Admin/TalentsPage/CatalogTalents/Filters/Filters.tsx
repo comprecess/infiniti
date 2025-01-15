3ff537d92abe9f9ac8d5afce2a93df65eb23a418
Jme Truffle <trufflecooperation@gmail.com>
2025-01-15 21:55:00 +0300

    fix

--- frontend/src/features/Admin/TalentsPage/CatalogTalents/Filters/Filters.tsx
+++ frontend/src/features/Admin/TalentsPage/CatalogTalents/Filters/Filters.tsx
@@ -16,13 +16,14 @@ import { CustomCheckBox } from '../../../../../shared/ui/CustomCheckBox/CustomCh
 import { CustomCheckBoxIndeterminate } from '../../../../../shared/ui/CustomCheckBoxIndeterminate/CustomCheckBoxIndeterminate'
 import { CustomDivider } from '../../../../../shared/ui/CustomDivider/CustomDivider'
 import { FromTo } from '../../../../../shared/ui/FromTo/FromTo'
-import { LoadingSpinner } from '../../../../../shared/ui/LoadingSpinner/LoadingSpinner'
 import { getPropertiesFiltering } from '../../../../../shared/utils/api/Client/Catalog/Properties/GetPropertiesFiltering'
 import { CategoryItem } from './CategoryItem/CategoryItem'
 import { Item } from './CategoryItem/Item/Item'
 import styles from './Filters.module.scss'
 
 interface FiltersProps {
+  filters: FiltersData[]
+  setFilters: Dispatch<SetStateAction<FiltersData[] | null>>
   selectedFilters: FiltersState
   setSort: Dispatch<SetStateAction<{ name: string; type: string }>>
   setActiveCategory: Dispatch<SetStateAction<number>>
@@ -30,13 +31,14 @@ interface FiltersProps {
 }
 
 export const Filters: FC<FiltersProps> = ({
+  filters,
+  setFilters,
   selectedFilters,
   setSelectedFilters,
   setActiveCategory,
   setSort,
 }) => {
   const [searchItems, setSearchItems] = useState<string[]>([])
-  const [filters, setFilters] = useState<FiltersData[] | null>(null)
 
   const { t } = useTranslation()
 
@@ -92,145 +94,136 @@ export const Filters: FC<FiltersProps> = ({
 
   return (
     <div className={styles.wrapper}>
-      {filters ? (
-        <>
-          <div className={styles.header}>
-            <h6 className={styles.title}>
-              {t('admin-catalog-talents-page-text-2')}
-            </h6>
-            <span
-              className={styles.buttonReset}
-              onClick={handleFiltersReset}
-            >
-              {t('admin-catalog-talents-page-text-3')}
-            </span>
-          </div>
-          <div className={styles.filters}>
-            {filters.map((filter, index) => (
-              <React.Fragment key={filter.id}>
-                {filter.type === 'checkbox' && (
-                  <>
-                    <CategoryItem
-                      isSearched
-                      title={filter.name}
-                      secondName={filter.values.length.toString()}
-                      handleSearchChange={value =>
-                        handleSearchChange(index, value)
-                      }
-                    >
-                      <Item
-                        categories={filter.values}
-                        searchItem={searchItems[index]}
-                        filters={selectedFilters}
-                        onCheckboxChange={handleCheckboxChange}
-                      />
-                    </CategoryItem>
-                    <CustomDivider />
-                  </>
-                )}
-                {filter.type === 'checkboxIndeterminate' && (
-                  <>
-                    <CategoryItem
-                      isSearched
-                      title={filter.name}
-                      secondName={filter.children.length.toString()}
-                      handleSearchChange={value =>
-                        handleSearchChange(index, value)
-                      }
-                    >
-                      <CustomCheckBoxIndeterminate
-                        languages={filter.children}
-                        filters={selectedFilters}
-                        searchItem={searchItems[index]}
-                        onCheckboxChange={handleCheckboxChange}
-                      />
-                    </CategoryItem>
-                    <CustomDivider />
-                  </>
-                )}
-                {filter.type === 'checkboxOnlyForValue' && (
-                  <>
-                    <CategoryItem title={filter.name} secondName='€ – EUR'>
-                      <div className={styles.items}>
-                        {filter.children.map(item => {
-                          return (
-                            <FromTo
-                              key={item.id}
-                              title={item.name}
-                              filters={selectedFilters}
-                              propId={String(item.id)}
-                              setSelectedFilters={setSelectedFilters}
-                              placeholderFirst={`from ${item.options.placeholder.from}`}
-                              placeholderSecond={`to ${item.options.placeholder.to}`}
-                            />
-                          )
-                        })}
-                        <CustomCheckBox
-                          title={filter.values[0].value}
-                          isChecked={
-                            selectedFilters[
-                              filter.values[0].propId
-                            ]?.includes(filter.values[0].id) || false
-                          }
-                          onChange={e =>
-                            handleCheckboxChange(
-                              filter.values[0].propId.toString(),
-                              filter.values[0].id,
-                              e.target.checked,
-                            )
-                          }
-                        />
-                      </div>
-                    </CategoryItem>
-                    <CustomDivider />
-                  </>
-                )}
-                {filter.type === null && (
-                  <>
-                    <CategoryItem title={filter.name}>
-                      <div className={styles.items}>
-                        {filter.children.map(item => {
-                          return (
-                            <FromTo
-                              key={item.id}
-                              title={item.name}
-                              filters={selectedFilters}
-                              propId={String(item.id)}
-                              setSelectedFilters={setSelectedFilters}
-                              placeholderFirst={`from ${item.options.placeholder.from}`}
-                              placeholderSecond={`to ${item.options.placeholder.to}`}
-                            />
-                          )
-                        })}
-                      </div>
-                    </CategoryItem>
-                    <CustomDivider />
-                  </>
-                )}
-                {filter.type === 'integer' && (
-                  <>
-                    <CategoryItem title={filter.name}>
-                      <div className={styles.items}>
+      <div className={styles.header}>
+        <h6 className={styles.title}>
+          {t('admin-catalog-talents-page-text-2')}
+        </h6>
+        <span className={styles.buttonReset} onClick={handleFiltersReset}>
+          {t('admin-catalog-talents-page-text-3')}
+        </span>
+      </div>
+      <div className={styles.filters}>
+        {filters.map((filter, index) => (
+          <React.Fragment key={filter.id}>
+            {filter.type === 'checkbox' && (
+              <>
+                <CategoryItem
+                  isSearched
+                  title={filter.name}
+                  secondName={filter.values.length.toString()}
+                  handleSearchChange={value =>
+                    handleSearchChange(index, value)
+                  }
+                >
+                  <Item
+                    categories={filter.values}
+                    searchItem={searchItems[index]}
+                    filters={selectedFilters}
+                    onCheckboxChange={handleCheckboxChange}
+                  />
+                </CategoryItem>
+                <CustomDivider />
+              </>
+            )}
+            {filter.type === 'checkboxIndeterminate' && (
+              <>
+                <CategoryItem
+                  isSearched
+                  title={filter.name}
+                  secondName={filter.children.length.toString()}
+                  handleSearchChange={value =>
+                    handleSearchChange(index, value)
+                  }
+                >
+                  <CustomCheckBoxIndeterminate
+                    languages={filter.children}
+                    filters={selectedFilters}
+                    searchItem={searchItems[index]}
+                    onCheckboxChange={handleCheckboxChange}
+                  />
+                </CategoryItem>
+                <CustomDivider />
+              </>
+            )}
+            {filter.type === 'checkboxOnlyForValue' && (
+              <>
+                <CategoryItem title={filter.name} secondName='€ – EUR'>
+                  <div className={styles.items}>
+                    {filter.children.map(item => {
+                      return (
                         <FromTo
-                          key={filter.id}
-                          setSelectedFilters={setSelectedFilters}
+                          key={item.id}
+                          title={item.name}
                           filters={selectedFilters}
-                          propId={String(filter.id)}
-                          placeholderFirst={`from ${filter.options.placeholder.from}`}
-                          placeholderSecond={`to ${filter.options.placeholder.to}`}
+                          propId={String(item.id)}
+                          setSelectedFilters={setSelectedFilters}
+                          placeholderFirst={`from ${item.options.placeholder.from}`}
+                          placeholderSecond={`to ${item.options.placeholder.to}`}
                         />
-                      </div>
-                    </CategoryItem>
-                    <CustomDivider />
-                  </>
-                )}
-              </React.Fragment>
-            ))}
-          </div>
-        </>
-      ) : (
-        <LoadingSpinner size='xl' />
-      )}
+                      )
+                    })}
+                    <CustomCheckBox
+                      title={filter.values[0].value}
+                      isChecked={
+                        selectedFilters[filter.values[0].propId]?.includes(
+                          filter.values[0].id,
+                        ) || false
+                      }
+                      onChange={e =>
+                        handleCheckboxChange(
+                          filter.values[0].propId.toString(),
+                          filter.values[0].id,
+                          e.target.checked,
+                        )
+                      }
+                    />
+                  </div>
+                </CategoryItem>
+                <CustomDivider />
+              </>
+            )}
+            {filter.type === null && (
+              <>
+                <CategoryItem title={filter.name}>
+                  <div className={styles.items}>
+                    {filter.children.map(item => {
+                      return (
+                        <FromTo
+                          key={item.id}
+                          title={item.name}
+                          filters={selectedFilters}
+                          propId={String(item.id)}
+                          setSelectedFilters={setSelectedFilters}
+                          placeholderFirst={`from ${item.options.placeholder.from}`}
+                          placeholderSecond={`to ${item.options.placeholder.to}`}
+                        />
+                      )
+                    })}
+                  </div>
+                </CategoryItem>
+                <CustomDivider />
+              </>
+            )}
+            {filter.type === 'integer' && (
+              <>
+                <CategoryItem title={filter.name}>
+                  <div className={styles.items}>
+                    <FromTo
+                      key={filter.id}
+                      setSelectedFilters={setSelectedFilters}
+                      filters={selectedFilters}
+                      propId={String(filter.id)}
+                      placeholderFirst={`from ${filter.options.placeholder.from}`}
+                      placeholderSecond={`to ${filter.options.placeholder.to}`}
+                    />
+                  </div>
+                </CategoryItem>
+                <CustomDivider />
+              </>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Admin/TanlentsPage/CatalogTalentsPage/CatalogTalentsPage.tsx
+++ frontend/src/pages/Admin/TanlentsPage/CatalogTalentsPage/CatalogTalentsPage.tsx
@@ -4,13 +4,21 @@ import { useTranslation } from 'react-i18next'
 import {
   FiltersData,
   FiltersState,
+  page,
+  PagesMetaData,
+  TalentData,
+  userTalentsPageString,
 } from '../../../../app/constants/constants'
 import { CategoriesItem } from '../../../../features/Admin/TalentsPage/CatalogTalents/CategoriesItem/CategoriesItem'
 import { Filters } from '../../../../features/Admin/TalentsPage/CatalogTalents/Filters/Filters'
 import { TalentsList } from '../../../../features/Admin/TalentsPage/CatalogTalents/TalentsList/TalentsList'
 import { TitlePage } from '../../../../features/Main/TitlePage/TitlePage'
+import { useCustomToast } from '../../../../shared/ui/CustomToast/CustomToast'
 import { LoadingSpinner } from '../../../../shared/ui/LoadingSpinner/LoadingSpinner'
+import { deleteSelectedTalent } from '../../../../shared/utils/api/Admin/Talents/DeleteTalent'
 import { getPropertiesFiltering } from '../../../../shared/utils/api/Client/Catalog/Properties/GetPropertiesFiltering'
+import { getUsersListInfo } from '../../../../shared/utils/api/Client/Catalog/User/GetUsersListInfo'
+import { getSession } from '../../../../shared/utils/Saving/Session/GetSession'
 import styles from './CatalogTalentsPage.module.scss'
 
 export const AdminCatalogTalentsPage: FC = () => {
@@ -19,7 +27,19 @@ export const AdminCatalogTalentsPage: FC = () => {
   const [sort, setSort] = useState({ name: 'priceDay', type: 'asc' })
   const [selectedFilters, setSelectedFilters] = useState<FiltersState>({})
 
+  const [talentsList, setTalentsList] = useState<{
+    data: TalentData[]
+    meta: PagesMetaData
+  } | null>(null)
+  const [currentPage, setCurrentPage] = useState<number>(
+    getSession(userTalentsPageString),
+  )
+
+  const [filters, setFilters] = useState<FiltersData[] | null>(null)
+
   const { t } = useTranslation()
+
+  const showToast = useCustomToast()
 
   const fetchCategories = useCallback(async () => {
     try {
@@ -28,6 +48,52 @@ export const AdminCatalogTalentsPage: FC = () => {
     } catch (error) {
       console.error('Failed to fetch categories:', error)
     }
+  }, [])
+
+  const fetchTalents = useCallback(async () => {
+    try {
+      const response = await getUsersListInfo(
+        page + String(currentPage),
+        selectedFilters,
+        sort,
+      )
+
+      setTalentsList(response)
+
+      if (currentPage > response.meta.last_page) {
+        setCurrentPage(1)
+      }
+    } catch (error) {
+      /* empty */
+    }
+  }, [currentPage, selectedFilters, sort])
+
+  const deleteTalent = useCallback(
+    async (idTalent: number) => {
+      const deleteResponse = await deleteSelectedTalent(idTalent)
+
+      if (deleteResponse.status) {
+        showToast({
+          title: 'Successfully',
+          description: 'You have successfully removed the Talent',
+          status: 'success',
+        })
+        fetchTalents()
+      } else {
+        showToast({
+          title: 'Error',
+          description: deleteResponse.message,
+          status: 'error',
+        })
+      }
+    },
+    [fetchTalents],
+  )
+
+  const getFilters = useCallback(async () => {
+    const filtersAnswer = await getPropertiesFiltering()
+
+    setFilters(filtersAnswer.data)
   }, [])
 
   const updateFilters = useCallback(() => {
@@ -59,54 +125,69 @@ export const AdminCatalogTalentsPage: FC = () => {
     updateFilters()
   }, [activeCategory, updateFilters])
 
+  useEffect(() => {
+    fetchTalents()
+  }, [fetchTalents])
+
+  useEffect(() => {
+    getFilters()
+  }, [getFilters])
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>
         <TitlePage title={t('admin-catalog-talents-page-title')} />
       </div>
-      {categories ? (
-        <section className={styles.sectionFirst}>
-          <div className={styles.itemsFirst}>
-            <span className={styles.categoriesText}>
-              {t('admin-catalog-talents-page-text-1')}
-            </span>
-            <div className={styles.categories}>
-              <CategoriesItem
-                name={t('admin-catalog-talents-page-text-5')}
-                isActive={activeCategory === 0}
-                onClick={() => setActiveCategory(0)}
-              />
-              {categories.values.map((category, index) => (
+      {categories && talentsList && filters ? (
+        <>
+          <section className={styles.sectionFirst}>
+            <div className={styles.itemsFirst}>
+              <span className={styles.categoriesText}>
+                {t('admin-catalog-talents-page-text-1')}
+              </span>
+              <div className={styles.categories}>
                 <CategoriesItem
-                  key={index + 1}
-                  name={category.value.replace(/&amp;/g, '&')}
-                  isActive={activeCategory === index + 1}
-                  onClick={() => setActiveCategory(index + 1)}
+                  name={t('admin-catalog-talents-page-text-5')}
+                  isActive={activeCategory === 0}
+                  onClick={() => setActiveCategory(0)}
                 />
-              ))}
+                {categories.values.map((category, index) => (
+                  <CategoriesItem
+                    key={index + 1}
+                    name={category.value.replace(/&amp;/g, '&')}
+                    isActive={activeCategory === index + 1}
+                    onClick={() => setActiveCategory(index + 1)}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section className={styles.sectionSecond}>
+            <div className={styles.itemsSecond}>
+              <Filters
+                filters={filters}
+                setFilters={setFilters}
+                selectedFilters={selectedFilters}
+                setSelectedFilters={setSelectedFilters}
+                setActiveCategory={setActiveCategory}
+                setSort={setSort}
+              />
+              <TalentsList
+                talentsList={talentsList}
+                sort={sort}
+                setSort={setSort}
+                deleteTalent={deleteTalent}
+                fetchTalents={fetchTalents}
+                setCurrentPage={setCurrentPage}
+              />
+            </div>
+          </section>
+        </>
       ) : (
         <div className={styles.loading}>
           <LoadingSpinner size='xl' />
         </div>
       )}
-      <section className={styles.sectionSecond}>
-        <div className={styles.itemsSecond}>
-          <Filters
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-            setActiveCategory={setActiveCategory}
-            setSort={setSort}
-          />
-          <TalentsList
-            sort={sort}
-            setSort={setSort}
-            selectedFilters={selectedFilters}
-          />
-        </div>
-      </section>
     </div>
   )
 }
