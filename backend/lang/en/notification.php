c653b40881b33404075fd889ef9aa5109ed4353a
Igor <crash.strelkov@gmail.com>
2025-04-16 21:24:27 +0400

    notification meeting fail

--- backend/lang/en/notification.php
+++ backend/lang/en/notification.php
@@ -1,5 +1,8 @@
 <?php
 
     return [
-        'Meeting' => "Do you have an appointment for the meeting, it will take place at :date"
+        'Meeting' => "You have an appointment for the meeting, it will take place on <b>:date</b> at the link <a href=\":link\">:link</a>",
+        'fail' => [
+            'Meeting' => "You have a meeting, but something went wrong"
+        ]
     ];
