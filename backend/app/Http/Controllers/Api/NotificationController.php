d50bb9515a37cb59dbe518eec1de18446d2e2181
Igor <crash.strelkov@gmail.com>
2025-05-08 17:29:45 +0400

    test

--- backend/app/Http/Controllers/Api/NotificationController.php
+++ backend/app/Http/Controllers/Api/NotificationController.php
@@ -50,15 +50,25 @@ class NotificationController extends Controller
     public function subscribePush(PushRequest $request)
     {
         $user = User::getAuth();
-        if(!$user->push){
-            $push = new Push();
-            $push->setUser($user);
+//        if(!$user->push){
+//            $push = new Push();
+//            $push->setUser($user);
+//
+//            $subscription = $request->subscription;
+//            $push->endpoint = Arr::get($subscription, 'endpoint');
+//            $push->keys = Arr::get($subscription, 'keys');
+//            $push->save();
+//        }
 
-            $subscription = $request->subscription;
-            $push->endpoint = Arr::get($subscription, 'endpoint');
-            $push->keys = Arr::get($subscription, 'keys');
-            $push->save();
-        }
+        $subscription = $request->subscription;
+        Push::updateOrCreate(
+            ['endpoint' => Arr::get($subscription, 'endpoint')],
+            [
+                'keys' => Arr::get($subscription, 'keys'),
+                'user_type' => $user::class,
+                'user_id' => $user->id
+            ]
+        );
 
         return response()->json(['success' => true]);
     }
