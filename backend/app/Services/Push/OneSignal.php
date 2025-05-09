5215d2bde828e917185539e16b3a394ac62714c4
Igor <crash.strelkov@gmail.com>
2025-05-09 15:38:19 +0400

    fix

--- backend/app/Services/Push/OneSignal.php
+++ backend/app/Services/Push/OneSignal.php
@@ -14,10 +14,17 @@ class OneSignal extends Push implements PushContract
 
     public function send(PushModel $model, $title, $message)
     {
-        OneSignalClient::sendNotificationToUser(
-            $message,
-            $model->hash
+//        OneSignalClient::sendNotificationToUser(
+//            $message,
+//            $model->hash
+//        );
+        $onesignal = new OneSignalClient(
+            env('ONESIGNAL_APP_ID'),
+            env('ONESIGNAL_REST_API_KEY'),
+            env('ONESIGNAL_USER_AUTH_KEY'),
         );
+
+        $onesignal->sendNotificationToUser($message, $model->hash);
     }
 
 }
