6f3a18b46c629ffdee67493b8d8712ae0dca9324
Igor <crash.strelkov@gmail.com>
2025-07-21 17:55:25 +0400

    fix CRUD isNew

--- backend/app/Models/Collection/TransactionCollection.php
+++ backend/app/Models/Collection/TransactionCollection.php
@@ -14,33 +14,33 @@ class TransactionCollection extends Collection
 
     public function profit()
     {
-        return $this->sum(function($item){
+        return round($this->sum(function($item){
             return $item->transformPrice('cr');
-        });
+        }), 2);
     }
 
     public function expense()
     {
-        return $this->sum(function($item){
+        return round($this->sum(function($item){
             return $item->transformPrice('dr');
-        });
+        }), 2);
     }
 
     public function amount($transformCurrency = false)
     {
-        return $this->sum(function($item) use($transformCurrency){
+        return round($this->sum(function($item) use($transformCurrency){
             return $transformCurrency ? $item->transformPrice('amount') : $item->amount;
-        });
+        }), 2);
     }
 
     public function amountByType(array $types = [], $transformCurrency = false)
     {
-        return $this->sum(function ($item) use($types, $transformCurrency){
+        return round($this->sum(function ($item) use($types, $transformCurrency){
             if(in_array($item->type, $types)) {
                 return $transformCurrency ? $item->transformPrice('amount') : $item->amount;
             }
             return 0;
-        });
+        }),2);
     }
 
     public function getNetWorth()
