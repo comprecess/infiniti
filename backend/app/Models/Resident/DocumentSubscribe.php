69d25a2a2a9f10362ad5e6cd4e9d08dd5612cde4
Igor <crash.strelkov@gmail.com>
2025-08-16 16:38:55 +0400

    delete subscribe file

--- backend/app/Models/Resident/DocumentSubscribe.php
+++ backend/app/Models/Resident/DocumentSubscribe.php
@@ -2,14 +2,8 @@
 
 namespace App\Models\Resident;
 
-use App\Models\Contracts\InsertDefaultValueInterface;
-use App\Models\Resident\Invoices\Invoice;
-use App\Models\Traits\InsertDefaultValueTrait;
-use App\Models\Traits\UserTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Traits\FileStorageTrait;
-use App\Models\Resident\Project\Project;
 
 class DocumentSubscribe extends Model
 {
