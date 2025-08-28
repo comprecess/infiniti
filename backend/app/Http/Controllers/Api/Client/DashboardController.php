e798039be6e6bea139b5c439de68c47973270abe
Igor <crash.strelkov@gmail.com>
2025-08-28 18:35:09 +0400

    dashboard and order

--- backend/app/Http/Controllers/Api/Client/DashboardController.php
+++ backend/app/Http/Controllers/Api/Client/DashboardController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Resident\Invoices\InvoiceListResource;
 use App\Http\Resources\Resident\Invoices\OfferListResource;
+use App\Http\Resources\Resident\Orders\OrderListClientResource;
 use App\Http\Resources\Resident\Transactions\TransactionsListResource;
+use App\Models\Config;
 use App\Models\User;
 
 
@@ -16,11 +18,20 @@ class DashboardController extends Controller
     {
         $user = User::getAuth();
 
-        $invoices = $user->invoices()->orderByDesc('id')->limit(10)->get();
-        $offers = $user->offers()->orderByDesc('id')->limit(10)->get();
-        $transactions = $user->transaction()->orderByDesc('id')->limit(10)->get();
+        $invoices = $user->invoices()->with(['getCurrencyIso', 'user', 'user.companyClient','user.group'])->orderByDesc('id')->limit(10)->get();
+        $offers = $user->offers()->with(['getCurrencyIso', 'user', 'user.companyClient','user.group'])->orderByDesc('id')->limit(10)->get();
+        $order = $user->orders()->with(['getCurrencyIso'])->orderByDesc('id')->limit(10)->get();
+
+        $hideExpense = Config::get('hide_expense_client');
+        if($hideExpense) {
+            $transactionQuery = $user->transactionPayer();
+        }else{
+            $transactionQuery = $user->transaction();
+        }
+        $transactions = $transactionQuery->orderByDesc('id')->limit(10)->get();
 
         return response()->json([
+            'order' => OrderListClientResource::collection($order),
             'invoice' => InvoiceListResource::collection($invoices),
             'offer' => OfferListResource::collection($offers),
             'transaction' => TransactionsListResource::collection($transactions),
--- backend/app/Models/Resident/Orders/Order.php
+++ backend/app/Models/Resident/Orders/Order.php
@@ -2,22 +2,77 @@
 
 namespace App\Models\Resident\Orders;
 
-use App\Models\Users\Client;
+use App\Models\Catalog\CartOrder;
+use App\Models\Contracts\InsertDefaultValueInterface;
+use App\Models\Resident\Invoices\Invoice;
+use App\Models\Traits\CurrencyTrait;
+use App\Models\Traits\HelperTrait;
+use App\Models\Traits\InsertDefaultValueTrait;
+use App\Models\Traits\UserTrait;
+//use App\Models\Users\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class Order extends Model implements InsertDefaultValueInterface
 {
-    use HasFactory;
+    use HasFactory, UserTrait, InsertDefaultValueTrait, HelperTrait, CurrencyTrait;
+
+    const STATUS = ['Pending', 'Active'];
 
     protected $table = "sys_orders";
+
+    public $timestamps = false;
 
     protected $casts = [
         'date_added' => 'date',
     ];
 
-    public function user()
+//    public function user()
+//    {
+//        return $this->belongsTo(Client::class, 'cid');
+//    }
+
+    public function model()
     {
-        return $this->belongsTo(Client::class, 'cid');
+        return $this->morphTo('model');
+    }
+
+    public function setModel(Model $model)
+    {
+        $this->model_type = $model::class;
+        $this->model_id = $model->id;
+    }
+
+    public function getDefault(): array
+    {
+        $this->setRandomNum('ordernum', 10, true, true);
+        return [
+            'status' => [self::STATUS[0]],
+            'date_added' => [now()],
+            'pid' => [0],
+            'iid' => [0],
+            'billing_cycle' => ['One Time']
+        ];
+    }
+
+    public static function createByCatalog(CartOrder $modelOrder)
+    {
+        $modelOrder->load(['model', 'model.client', 'cart', 'cart.getCurrencyIso']);
+        $order = self::newDefault();
+        $order->setModel($modelOrder);
+
+        $model = $modelOrder->model;
+
+        if($client = $model?->client) {
+            $order->cname = $client->account;
+            $order->cid = $client->id;
+        }
+
+        $order->iid = $model instanceof Invoice ? $model->id : 0;
+        $order->amount = $modelOrder->cart->total;
+        $order->setCurrency($modelOrder->cart->getCurrencyIso) ;
+        $order->save();
+
+        return $order;
     }
 }
