d2deb5ba29f225ee5175b8ca2b2e868fdaf4fa12
Igor <crash.strelkov@gmail.com>
2025-10-15 19:17:05 +0300

    fix notification

--- backend/lang/en/pay.php
+++ backend/lang/en/pay.php
@@ -2,5 +2,5 @@
 
 return [
     'notFound' => 'Payment system ":name" not found',
-    'adminNotification' => 'Поступила новая оплата, тип оплаты: :type, от кого: <a href="/admin/customers/view/:id/summary">:account</a> подробно:<a href="/admin/sales/invoice/view/:id2">:code</a>'
+    'adminNotification' => 'A new payment has been received, payment type: :type, from: <a href="/admin/customers/view/:id/summary">:account</a> details: <a href="/admin/sales/invoice/view/:id2">:code</a>'
 ];
