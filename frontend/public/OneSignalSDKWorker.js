72e47ae4406380ce46322be932a23dbe2b0edabd
Jme Truffle <trufflecooperation@gmail.com>
2025-10-30 19:19:51 +0300

    chore: small update [patch]

--- frontend/public/OneSignalSDKWorker.js
+++ frontend/public/OneSignalSDKWorker.js
@@ -1,18 +1,20 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js')
 
 try {
-  const APP_VERSION = '0.0.15-alpha.15nl'
+  const APP_VERSION = '0.0.16-alpha.16jz'
   const CACHE_NAME = `infiniti-${APP_VERSION}`
   const ASSETS_TO_CACHE = ['/']
 
   self.addEventListener('install', event => {
     console.log('[INF SW] Installing...')
+
     event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)))
     self.skipWaiting()
   })
 
   self.addEventListener('activate', event => {
     console.log('[INF SW] Activating...')
+
     event.waitUntil(
       (async () => {
         const keys = await caches.keys()
