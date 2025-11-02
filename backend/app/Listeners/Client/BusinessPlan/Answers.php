c4f8b90f5dc63d24dc23cfb3ece23eb6bf5dc48f
Igor <crash.strelkov@gmail.com>
2025-11-02 13:40:49 +0300

    Answers value

--- backend/app/Listeners/Client/BusinessPlan/Answers.php
+++ backend/app/Listeners/Client/BusinessPlan/Answers.php
@@ -4,8 +4,10 @@ namespace App\Listeners\Client\BusinessPlan;
 
 use App\Events\Client\BusinessPlan\Generate;
 use App\Models\Resident\BusinessPlan;
+use App\Models\Resident\Question;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 
 class Answers implements  ShouldQueue
@@ -26,10 +28,24 @@ class Answers implements  ShouldQueue
         $event->businessPlan->status_generate = BusinessPlan::STATUS_GENERATE[1];
         $event->businessPlan->save();
 
-        for($i = 0; $i < 20; $i++) {
-            sleep(1);
+        $original = Arr::get($event->businessPlan->answer, 'original', []);
+        $chatGptAnswer = [];
+
+        if($original){
+            $questionList = Question::whereIn('id', array_keys($original))->get();
+
+            foreach($original as $id => $value){
+                $question = $questionList->where('id', $id)->first();
+                $chatGptAnswer[__($question->key_lang)] = $question->getValue($value);
+            }
         }
 
-        Log::alert('***Answers*** complete');
+        $event->businessPlan->answer['values'] = $chatGptAnswer;
+
+//        for($i = 0; $i < 20; $i++) {
+//            sleep(1);
+//        }
+//
+//        Log::alert('***Answers*** complete');
     }
 }
