b6693343ff13f77b44296c9e950d47c6f465234f
Igor <crash.strelkov@gmail.com>
2025-11-04 15:21:22 +0300

    google oauth

--- backend/app/Http/Controllers/Api/SocialController.php
+++ backend/app/Http/Controllers/Api/SocialController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Users\Admin;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Arr;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\InvalidStateException;
 use Illuminate\Http\Request;
@@ -15,7 +18,6 @@ class SocialController extends Controller
     private function getState($url)
     {
         $queryParse = parse_url($url);
-        dd($queryParse);
         parse_str($queryParse['query'], $params);
 
         return  $params['state'];
@@ -28,17 +30,30 @@ class SocialController extends Controller
 
     public function auth(Request $request)
     {
-
-
+        $error = false;
+        $user = null;
         try{
             $user = Socialite::driver('google')->user();
         }catch (InvalidStateException $e) {
+            $error = true;
+        }catch (ClientException $e){
+            $error = true;
+        }
+
+        if($error || !$user) {
             return redirect('/google/auth/');
         }
 
+        dd($user, $user->getEmail(), $user->getName(), $user->getNickname());
+
         $type = Cache::get($this->getKey($request->state)) ?? 'client';
 
-        return redirect('/google/auth/');
+        if($type == 'admin') {
+//            Admin::where('username', $user)
+        }
+
+
+
     }
 
     public function redirect(Request $request)
@@ -48,7 +63,7 @@ class SocialController extends Controller
 
         Cache::put($this->getKey($this->getState($redirectUrl)), $request->admin == 'admin' ? 'admin' : 'client', 30*60);
 
-        return $redirect;
+//        return $redirect;
         return response()->json([
             'redirect' => $redirectUrl,
         ]);
