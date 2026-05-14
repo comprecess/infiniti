ab2609c3f9d20eeaf09e3baeed7b05bb04b66e98
Paul Dealman <dealbroker@Pauls-MacBook-Air.local>
2026-05-15 00:09:31 +0300

    fix(tickets): fix custom-fetch and get-auth-token import paths

--- frontend/src/shared/utils/api/Admin/Tickets/get-tickets-list.ts
+++ frontend/src/shared/utils/api/Admin/Tickets/get-tickets-list.ts
@@ -1,6 +1,6 @@
 import { AUTH_ERROR_MESSAGE, NETWORK_ERROR_MESSAGE, REQUEST_TIMEOUT_MS } from '../../../../../app/constants/constants'
-import { customFetch } from '../../../custom-fetch'
-import { getAuthToken } from '../../../get-auth-token'
+import { customFetch } from '../../custom-fetch'
+import { getAuthToken } from '../../get-auth-token'
 
 interface Filters {
   status?: string
